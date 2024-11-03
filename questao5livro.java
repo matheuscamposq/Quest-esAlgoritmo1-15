3639f07f7b36fdd8698fcb90f25afbbd43ee8e45
matheuscamposq <ifmatheuscampos@gmail.com>
2024-11-03 13:36:29 -0300

    Update questao5livro.java

--- questao5livro.java
+++ questao5livro.java
@@ -1,4 +1,25 @@
+/**
+inicio
+    variáveis
+    inteiro frangos
+    real custo
+    
+    escreva("Insira a quantidade de frangos contidos na granja:")
+    leia(s_frangos)
+    
+    frangos := converte_para_inteiro(s_frangos)
+    custo := frangos * 11
+    
+    escreva("O valor total gasto com os frangos é: ", arredonda(custo, 2), "R$")
+    
+fim
 
+TESTE
+
+Entrada	frangos	custo
+100	     100	    1100.00 R$
+200	     200	  2200.00 R$
+**/
 package questao5livro;
 
 import javax.swing.JOptionPane;
