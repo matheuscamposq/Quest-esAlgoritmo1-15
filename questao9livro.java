1004cd8f1f5598506af4b013cf09ccf9af001c31
matheuscamposq <ifmatheuscampos@gmail.com>
2024-11-03 13:44:02 -0300

    Update questao9livro.java

--- questao9livro.java
+++ questao9livro.java
@@ -1,3 +1,36 @@
+/** 
+inicio
+    variáveis
+    real hpessoa, spessoa, spredio, hpredio
+    
+    escreva("Insira a altura da pessoa em metros:")
+    leia(s_hpessoa)
+    
+    escreva("Insira o tamanho da sombra em metros:")
+    leia(s_spessoa)
+    
+    escreva("Insira o tamanho da sombra do predio em metros:")
+    leia(s_spredio)
+    
+    tente
+        hpessoa := converte_para_real(s_hpessoa)
+        spessoa := converte_para_real(s_spessoa)
+        spredio := converte_para_real(s_spredio)
+        
+        hpredio := (hpessoa * spredio) / spessoa
+        
+        escreva("A altura do predio é ", arredonda(hpredio, 2), " m.")
+    capture (NumberFormatException e)
+        escreva("Por favor, insira um valor numérico válido")
+    fimtente
+    
+fim
+
+Entrada	       hpessoa	     spessoa        spredio	        hpredio	         Saída
+1.75	           1.75	      1.0	         2.0	           3.50	            A altura do prédio é 3.50 m.
+1.60	           1.60	      0.8	          1.6            	3.20            	A altura do prédio é 3.20 m. 
+**/
+
 package questao9livro;
 import javax.swing.JOptionPane;
 
