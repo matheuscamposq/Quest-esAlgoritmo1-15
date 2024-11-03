325e709d301eafa9f9007e9c880133293774528c
matheuscamposq <ifmatheuscampos@gmail.com>
2024-11-03 13:32:32 -0300

    Update questao3livro.java

--- questao3livro.java
+++ questao3livro.java
@@ -1,4 +1,24 @@
+/**
+inicio
+    variáveis
+    real c, f
+    
+    escreva("Insira a temperatura em Celsius:")
+    leia(s_c)
+    
+    c := converte_para_real(s_c)
+    f := (c * 1.8) + 32
+    
+    escreva("Temperatura em Celsius: ", c, "°C")
+    escreva("Temperatura em Fahrenheit: ", f, "°F")
+    
+fim
 
+TESTE DE MESA
+Entrada      	c      	f
+0	            0	     32.00
+25	           25	     77.00
+**/
 package questao3livro;
 
 import javax.swing.JOptionPane;
