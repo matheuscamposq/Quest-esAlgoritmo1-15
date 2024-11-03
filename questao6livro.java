61f41891e09bbbf7298b5272aa11b984542e6434
matheuscamposq <ifmatheuscampos@gmail.com>
2024-11-03 13:38:28 -0300

    Update questao6livro.java

--- questao6livro.java
+++ questao6livro.java
@@ -1,4 +1,33 @@
+/**
+inicio
+    variáveis
+    inteiro novelos, blusas
+    real calculo
+    
+    escreva("Informe a quantidade de blusas para a confecção:")
+    leia(s_blusas)
+    
+    escreva("Informe a quantidade total de novelos:")
+    leia(s_novelos)
+    
+    blusas := converte_para_inteiro(s_blusas)
+    novelos := converte_para_inteiro(s_novelos)
+    
+    se (blusas > 0) entao
+        calculo := novelos / blusas
+        escreva("Serão usados ", arredonda(calculo, 2), " novelos por blusa.")
+    senao
+        escreva("ATENÇÃO!!! O número de blusas deve ser maior que 0.")
+    fimse
+    
+fim
 
+Entrada      	blusas	     novelos	      calculo	       Saída
+5	              5	           10	            2.00	        Serão usados 2.00 novelos por blusa
+3	              3	           15	            5.00	        Serão usados 5.00 novelos por blusa
+0	              0	           10	            ---	            ATENÇÃO!!! O número de blusas deve ser maior que 0
+
+**/
 package questao6livro;
 
 import javax.swing.JOptionPane;
