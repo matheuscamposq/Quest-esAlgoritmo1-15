60e70f6bf642a5ea7b10cc99ec5c1f050ed81a2d
matheuscamposq <ifmatheuscampos@gmail.com>
2024-11-03 13:35:13 -0300

    Update questao4livro.java

--- questao4livro.java
+++ questao4livro.java
@@ -1,4 +1,31 @@
+/**
+inicio
+    variáveis
+    real hnormal, hextra, salariob, salarioL, desconto
+    
+    escreva("Insira o total de horas normais trabalhadas:")
+    leia(s_hnormal)
+    
+    escreva("Insira o total de horas extras trabalhadas:")
+    leia(s_hextra)
+    
+    hnormal := converte_para_real(s_hnormal)
+    hextra := converte_para_real(s_hextra)
+    
+    salariob := (hnormal * 10) + (hextra * 15)
+    desconto := salariob * 0.1
+    salarioL := salariob - desconto
+    
+    escreva("Salário bruto: ", salariob, "R$")
+    escreva("Salário líquido: ", salarioL, "R$")
+    
+fim
+TESTE DE MESA
+Entrada	                   hnormal	           hextra	        salariob	desconto	salarioL
+160 horas normais	         160	             10	            1750.00	       175.00	1575.00
+180 horas normais	         180	             20	            1950.00	       195.00	1755.00
 
+**/
 package questao4livro;
 
 import javax.swing.JOptionPane;
